9cf880389fcaaafbcf8a64ec3e4d54abeea22245
Andrus <andrus131@hotmail.com>
2020-09-26 14:43:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int a = 1;
+    int b = 4;
+    int c = 6;
+
+    int result1 = (c - b - a);
+        System.out.println("c - b - a = " + result1);
+
+    long d = 10;
+    long e = 20;
+    long f = 30;
+
+    long result2 = (d * e / f);
+        System.out.println("(d * e / f) = " + result2);
+
+
+
+    }
+}
